5f796b3d149a98d5846ffabf54b701663a83e9dd
mimi89999 <michel@lebihan.pl>
2019-08-25 22:27:38 +0200

    Added a task with SQL injection in cookie

---
+++ tasks/cookie_sqli/config.php
@@ -0,0 +1,10 @@
+<?php
+$db_server = '172.20.0.2';
+
+$db_username = 'cookie_sqli';
+$db_password = 'h6LEupySib86eMna5w9BuRzD0sm2vI';
+
+$db_name = 'cookie_sqli';
+
+// admin : JQh8uXef9QaffpACOOizx527Y4EVxw
+?>
