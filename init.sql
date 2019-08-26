c60a1670731dadd4604e658445a35d734b140fee
mimi89999 <michel@lebihan.pl>
2019-08-26 12:54:30 +0200

    Create database for cookie_sqli

---
+++ init.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE cookie_sqli;
+\c cookie_sqli;
+
+CREATE TABLE users (
+    id SERIAL NOT NULL PRIMARY KEY,
+    username varchar(255) NOT NULL UNIQUE,
+    password varchar(60) NOT NULL
+);
+
+CREATE TABLE sessions (
+    user_id INT NOT NULL REFERENCES users(id),
+    session VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE data (
+    user_id INT NOT NULL UNIQUE REFERENCES users(id),
+    data TEXT
+);
+
+CREATE ROLE cookie_sqli WITH LOGIN PASSWORD 'h6LEupySib86eMna5w9BuRzD0sm2vI';
+
+GRANT SELECT ON TABLE users, sessions, data TO cookie_sqli;
+GRANT INSERT ON TABLE sessions TO cookie_sqli;
+
+INSERT INTO users (id, username, password) VALUES
+    (1, 'admin', '$2y$10$HE570XU.gSfl.g0mJE8e0eBfilxpaUqx9PZwbbFyd1JbfaS7mAyjm'),
+    (2, 'test', '$2y$10$.QmgCZeIjFbXTcthnLyHQeHximojL3iJMr.tPphGkl2sP70JU77qS');
+
+INSERT INTO sessions (user_id, session) VALUES
+    (2, 'e75958c61727b1c5ad77e631f15633'),
+    (2, '70c6609d1323bce5c4f5ccab5067fb'),
+    (1, 'e60287e730f29937230255ade1b696');
+
+INSERT INTO data (user_id, data) VALUES
+    (1, 'wwi{1_pR3Fer_n0SQL:P}'),
+    (2, 'Nothing quite interesting here...');
--- tasks/cookie_sqli/config.php
+++ tasks/cookie_sqli/config.php
@@ -5,6 +5,4 @@ $db_username = 'cookie_sqli';
 $db_password = 'h6LEupySib86eMna5w9BuRzD0sm2vI';
 
 $db_name = 'cookie_sqli';
-
-// admin : JQh8uXef9QaffpACOOizx527Y4EVxw
 ?>
